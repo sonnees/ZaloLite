0a143990380c47905fa44e60e17346a2e3364938
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2024-01-11 00:50:32 +0700

    update layout

--- frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/index.jsx
+++ frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/index.jsx
@@ -1,16 +1,34 @@
+import React, { useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 function DefaultLayout({ children }) {
+  const [selectedItem, setSelectedItem] = useState("UuTien");
+
+  const handleItemSelected = (value) => {
+    setSelectedItem(value);
+  };
+
   return (
     <div className="flex h-screen w-full">
-      <div className="flex w-full">
+      <div className="flex h-screen w-full">
         <Navbar />
-        <Sidebar />
+        <div className="ml-16 flex w-full flex-col border md:w-[345px]">
+          <Sidebar onItemSelected={handleItemSelected} />
+          <div className="flex-1 overflow-y-auto text-gray-500">
+            {selectedItem === "UuTien" ? (
+              <div className="flex-1 overflow-y-auto text-gray-500">
+                {children}
+              </div>
+            ) : (
+              <div className="text-gray-500">This is content for Khac</div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className=" hidden w-full md:flex">
-        <div className=" text-gray-500">{children}</div>
+      <div className="hidden w-full md:flex">
+        <div className="text-gray-500">This is content</div>
       </div>
     </div>
   );
--- frontend/Web/zalo-lite/src/pages/Contact/index.jsx
+++ frontend/Web/zalo-lite/src/pages/Contact/index.jsx
@@ -1,8 +1,43 @@
 function Contact() {
     return (
-        <div>
-            <h1>This is contact</h1>
-        </div>
+      <div>
+        <h1>This is contact</h1>
+        <p>
+          1. Thể thức văn bản bắt buộc dùng phông chữ Times New Roman Nếu như
+          trong thể thức văn bản trước đây, phông chữ sử dụng để trình bày văn
+          bản trên máy vi tính là phông chữ tiếng Việt của bộ mã ký tự Unicode
+          theo Tiêu chuẩn Việt Nam TCVN 6909:2001 thì hiện nay đã quy định cụ
+          thể phông chữ phải là phông chữ tiếng Việt Times New Roman, bộ mã ký
+          tự Unicode theo Tiêu chuẩn Việt Nam TCVN 6909:2001. Cỡ chữ và kiểu chữ
+          không có quy định chung mà phụ thuộc vào từng yếu tố thể thức. 2. Chỉ
+          sử dụng khổ giấy A4 cho tất cả các loại văn bản Thay vì được phép
+          trình bày văn bản hành chính trên khổ giấy A4 hoặc A5 (đối với giấy
+          giới thiệu, giấy biên nhận hồ sơ, phiếu gửi, phiếu chuyển) thì hiện
+          nay, tất cả các loại văn bản hành chính đều chỉ sử dụng chung khổ giấy
+          A4 (210mm x 297mm). Văn bản được trình bày theo chiều dài của khổ A4,
+          trường hợp văn bản có các bảng, biểu nhưng không được làm thành phụ
+          lục riêng thì có thể được trình bày theo chiều rộng. 3. Thay đổi cách
+          đánh số trang văn bản Trước đây, số trang văn bản được trình bày tại
+          góc phải ở cuối trang giấy (phần footer) thì theo thể thức văn bản mới
+          nhất hiện nay, số trang văn bản được đặt canh giữa theo chiều ngang
+          trong phần lề trên của văn bản, được đánh từ số 1, bằng chữ số Ả-rập,
+          cỡ chữ 13 đến 14, kiểu chữ đứng và cũng không hiển thị số trang thứ
+          nhất. 4. Phải ghi cả tên cơ quan chủ quản Thông tư 01 loại trừ một số
+          trường hợp không ghi cơ quan chủ quản thì nay quy định mới đã bãi bỏ
+          các trường hợp loại trừ này. Tên cơ quan, tổ chức ban hành văn bản là
+          tên chính thức, đầy đủ của cơ quan, tổ chức hoặc chức danh nhà nước
+          của người có thẩm quyền ban hành văn bản. Tên cơ quan, tổ chức ban
+          hành văn bản bao gồm: tên cơ quan, tổ chức ban hành văn bản và tên cơ
+          quan, tổ chức chủ quản trực tiếp. Đối với tên cơ quan chủ quản trực
+          tiếp ở địa phương phải có thêm tên tỉnh, thành phố trực thuộc Trung
+          ương hoặc quận, huyện, thị xã, thành phố hoặc xã, phường, thị trấn nơi
+          cơ quan đóng trụ sở. Được phép viết tắt những cụm từ thông dụng. Tên
+          cơ quan ban hành văn bản được trình bày chữ in hoa, cỡ chữ 12 tới 13,
+          đứng, đậm, đặt canh giữa dưới tên cơ quan chủ quản trực tiếp. Trong
+          đó, tên cơ quan chủ quản trực tiếp viết chữ in hoa, đứng, cỡ chữ 12
+          tới 13.
+        </p>
+      </div>
     );
 }
 
