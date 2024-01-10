81c939619fb024824ad4160e3a7e57efff229466
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2024-01-10 20:39:14 +0700

    update navbar

--- frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/Navbar/index.jsx
+++ frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/Navbar/index.jsx
@@ -1,57 +1,72 @@
-import { Routes, Route, Link } from "react-router-dom";
-
-import { publicMainHomeRoutes } from "../../../../routes";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+
+  let messageImage = "/message-outline.png";
+  let contactImage = "/contact-book-outline.png";
+  let todoImage = "/todo-outline.png";
+
+  if (location.pathname === "/") {
+    messageImage = "/message.png";
+  }
+  if (location.pathname === "/contact") {
+    contactImage = "/contact-selected.png";
+  }
+  if (location.pathname === "/todo") {
+    todoImage = "/todo-selected.png";
+  }
   return (
-    <div className="flex w-[64px] bg-[#0091ff]  p-3 pt-[32px]  ">
-      <nav className="nav__tabs flx flx-col flx-sp-btw ">
-        <ul>
-          <li>
+    <div className="fixed h-full w-16 bg-[#0091ff]  pt-8">
+      <nav className="w-full">
+        <ul className="grid w-full items-center justify-center">
+          <li className="px-2 pb-5">
             <img
               src="https://s120-ava-talk.zadn.vn/2/5/a/5/6/120/5ded83a5856f6d2af9fce6eac4b8d6d2.jpg"
-              className="rounded-[50%] border"
+              className="w-14 rounded-full border "
               alt="avatar"
             />
           </li>
           <li>
-            <Link to="/">
+            <Link
+              to="/"
+              className={`flex justify-center p-4 py-5 ${
+                location.pathname === "/" ? "bg-[#006edc]" : ""
+              }`}
+            >
               <img
-                src="/message.png"
-                className="h-[30px] w-[30px]"
+                src={messageImage}
+                className="h-[24px] w-[24px] items-center justify-center"
                 alt="avatar"
               />
             </Link>
           </li>
           <li>
-            <Link to="/contact">
+            <Link
+              to="/contact"
+              className={`flex justify-center p-4 py-5 ${
+                location.pathname === "/contact" ? "bg-[#006edc]" : ""
+              }`}
+            >
               <img
-                src="/contact-book.png"
-                className="h-[30px] w-[30px]"
+                src={contactImage}
+                className="h-[24px] w-[24px]"
                 alt="avatar"
               />
             </Link>
           </li>
           <li>
-            <Link to="/todo">
-              <img
-                src="/checkbox.png"
-                className="h-[30px] w-[30px]"
-                alt="avatar"
-              />
+            <Link
+              to="/todo"
+              className={`flex justify-center p-4 py-5 ${
+                location.pathname === "/todo" ? "bg-[#006edc]" : ""
+              }`}
+            >
+              <img src={todoImage} className="h-[22px] w-[22px]" alt="avatar" />
             </Link>
           </li>
         </ul>
       </nav>
-      {/* <Routes>
-                {publicMainHomeRoutes.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        element={<route.component />}
-                    />
-                ))}
-            </Routes> */}
     </div>
   );
 }
--- frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/index.jsx
+++ frontend/Web/zalo-lite/src/components/Layout/DefaultLayout/index.jsx
@@ -3,19 +3,17 @@ import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 function DefaultLayout({ children }) {
-    return (
-        <div className=" container w-full h-screen flex ">
-            <div className="flex flex-wrap bg-gray-100 ">
-                <Navbar />
-                <Sidebar />
-            </div>
-            <div className="flex flex-wrap">
-                <div className="p-4 text-gray-500">
-                    {children}
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex w-full">
+        <Navbar />
+        <Sidebar />
+      </div>
+      <div className=" hidden w-full md:flex">
+        <div className=" text-gray-500">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
