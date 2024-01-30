284b5786c7a232ac3edb010818ed8571af4c523a
LeHuuBang2002 <huubangle20002@gmail.com>
2024-01-30 14:01:35 +0700

    update profile

--- frontend/App/App.js
+++ frontend/App/App.js
@@ -5,6 +5,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 
 import login from './screens/LoginScreen';
+import ChatApp from './screens/ChatApp';
+import Profile from './screens/Profile';
 
 export default function App() {
   let Stack = createStackNavigator();
@@ -12,7 +14,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="login" component={login}  options={{headerShown: false}}/>
+        {/* <Stack.Screen name="login" component={login}  options={{headerShown: false}}/> */}
+        {/* <Stack.Screen name="ChatApp" component={ChatApp} options={{headerShown: false}}/> */}
+        <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ frontend/App/screens/Person.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Person() {
+  return (
+    <View>
+      <Text>Person</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
