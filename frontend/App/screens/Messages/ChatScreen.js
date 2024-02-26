06f285966707f62b0f00f86c5c1a8ee554c8f78f
Lê Hữu Bằng <105543973+bthope@users.noreply.github.com>
2024-02-26 18:09:12 +0700

    Huu bang (#27)
    * Update UI Fontand
    
    * Update UIChat
    
    * Update UIChat App
    
    * Update UI Intalls and InformationDetail
    
    * Update IntallUI
    
    * updata App
    
    * Updata FontApp/
    
    * update App fontend
    
    * fix merge
    
    * fix checout

---
+++ frontend/App/screens/Messages/ChatScreen.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Modal, View, Text, Button, SafeAreaView } from 'react-native';
+
+const MyModal = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        
+    </SafeAreaView>
+  );
+};
+
+export default MyModal;
