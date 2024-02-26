7a3787ed7bd27c6fd7bdda4890a2624a25ba9fea
LeHuuBang2002 <huubangle20002@gmail.com>
2024-02-26 09:33:28 +0700

    Updata FontApp/

--- frontend/App/App.js
+++ frontend/App/App.js
@@ -3,14 +3,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
-import login from './screens/LoginScreen';
-import ChatApp from './screens/ChatApp';
 import Profile from './screens/Profile';
 import Person from './screens/Person';
 import TranferAccount from './screens/TranferAccount';
 import Diary from './screens/Diary';
 import Information from "./screens/Information";
+import InformationDetail from "./screens/InformationDetail";
+import Install from "./screens/Install";
+
 
 export default function App() {
   let Stack = createStackNavigator();
@@ -18,14 +18,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="login" component={login}  options={{headerShown: false}}/> */}
-        {/* <Stack.Screen name="ChatApp" component={ChatApp} options={{headerShown: false}}/> */}
+        
+        <Stack.Screen name="Person" component={Person} options={{headerShown: false}}/>
+        <Stack.Screen name="Install" component={Install} options={{headerShown: false}}/>
+        <Stack.Screen name="InformationDetail" component={InformationDetail} options={{headerShown: false}}/>
 
         <Stack.Screen name="Information" component={Information} options={{headerShown: false}}/>
         <Stack.Screen name="Diary" component={Diary} options={{headerShown: false}}/>
-        <Stack.Screen name="Person" component={Person} options={{headerShown: false}}/>
+        
         <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-        <Stack.Screen name="TranferAccount" component={TranferAccount} options={{headerShown: false}}/> 
+        <Stack.Screen name="TranferAccount" component={TranferAccount} options={{headerShown: false}}/>
         
       </Stack.Navigator>
     </NavigationContainer>
