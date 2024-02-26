2edb33539010859e635c9fecfba01d6c03b574cd
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2024-02-26 12:10:33 +0700

    commit GUI

--- frontend/Web/zalo-lite/src/pages/Contact/index.jsx
+++ frontend/Web/zalo-lite/src/pages/Contact/index.jsx
@@ -1,43 +1,43 @@
+import { faAddressBook, faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
+import { faMobileScreen, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { useState } from "react";
+
 function Contact() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <div className="h-full w-full flex-1 flex-col">
-      <div></div>
-      <div className="flex-1 pl-4 ">
-        <div className="h-screen w-full overflow-auto">
-          <div className="flex w-full items-center pr-2">
-            <div className="transition-min-width flex min-w-[calc(100vw-600px)] md:min-w-[306px] items-center text-[#7589A3] duration-200">
-              <p className="text-justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia
-                iste praesentium beatae eum quibusdam minus! Fugit incidunt
-                aliquam recusandae facilis cupiditate temporibus nam, molestias
-                ea! Repudiandae ratione vel eius voluptatem. Lorem ipsum dolor
-                sit amet consectetur, adipisicing elit. Nemo nisi sunt at
-                nostrum facilis, perspiciatis ut amet. Repellat recusandae
-                dolorem laudantium voluptatum aliquid veritatis, dolore
-                blanditiis, et ducimus porro sed. Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Reprehenderit, sit nobis. Sunt, a
-                quis iusto commodi distinctio vitae quasi ut quae culpa, omnis
-                voluptatum earum inventore magnam assumenda itaque facere. Lorem
-                ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-                cupiditate autem dolores ea distinctio facere ipsa commodi odio
-                eos, saepe expedita tempora dicta illo molestias cum fugit quos
-                iure cumque. Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Ipsa illum recusandae placeat maiores dolorum magnam,
-                labore quod? Rerum voluptatibus sed, nulla maxime atque nobis
-                ut. Necessitatibus quasi saepe maiores incidunt! Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Dolorem dignissimos
-                sed beatae accusamus ad nihil corporis blanditiis sint, quisquam
-                aut, rerum, perferendis animi temporibus incidunt porro magni!
-                Sequi, repellendus veritatis. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Est quidem fugiat ullam eum
-                reprehenderit minus reiciendis porro dolores facere vitae sequi
-                officia voluptatum, optio maiores deleniti officiis nisi, iusto
-                amet.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="h-[64px] w-[334px] flex-col checked:text-red-700">
+    <List className="p-0 my-0">
+      <ListItemButton className="h-[64px]" id="listFriend" selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
+        <ListItemIcon>
+          <FontAwesomeIcon icon={faAddressBook} className='mx-3'/>
+        </ListItemIcon>
+        <ListItemText primary="Danh sách bạn bè"/>
+      </ListItemButton>
+
+      <ListItemButton className="h-[64px]" id="listGroup" selected={selectedIndex === 1} onClick={(event) => handleListItemClick(event, 1)}>
+        <ListItemIcon>
+          <FontAwesomeIcon icon={faUserGroup} className='mx-3'/>
+        </ListItemIcon>
+        <ListItemText primary="Danh sách nhóm"/>
+      </ListItemButton>
+
+      <ListItemButton className="h-[64px]" id="addFriend" selected={selectedIndex === 2} onClick={(event) => handleListItemClick(event, 2)}>
+        <ListItemIcon>
+          <FontAwesomeIcon icon={faEnvelopeOpen} className='mx-3'/>
+        </ListItemIcon>
+        <ListItemText primary="Lời mời kết bạn"/>
+      </ListItemButton>
+
+    </List>
+      
+
     </div>
   );
 }
