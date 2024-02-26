a9f9687fe8e1bbc3fde01ad40a622c036fdd1013
LeHuuBang2002 <huubangle20002@gmail.com>
2024-02-26 09:22:21 +0700

    updata App

--- frontend/App/App.js
+++ frontend/App/App.js
@@ -3,15 +3,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
-import login from './screens/LoginScreen';
-<<<<<<< HEAD
-import ChatApp from './screens/ChatApp';
 import Profile from './screens/Profile';
 import Person from './screens/Person';
-import AccountTranfer from './screens/AccountTranfer';
-=======
->>>>>>> 4f24236f617f4f6f5d1c142eccd7fb702c07ad1f
+import TranferAccount from './screens/TranferAccount';
+import Diary from './screens/Diary';
+import Information from "./screens/Information";
+import InformationDetail from "./screens/InformationDetail";
+import Install from "./screens/Install";
+
 
 export default function App() {
   let Stack = createStackNavigator();
@@ -19,16 +18,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-<<<<<<< HEAD
-        {/* <Stack.Screen name="login" component={login}  options={{headerShown: false}}/> */}
-        {/* <Stack.Screen name="ChatApp" component={ChatApp} options={{headerShown: false}}/> */}
-        <Stack.Screen name="AccountTranfer" component={AccountTranfer} options={{headerShown: false}}/>
-        <Stack.Screen name="Person" component={Person} options={{headerShown: false}}/>
-        <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
         
-=======
-        <Stack.Screen name="login" component={login}  options={{headerShown: false}}/>
->>>>>>> 4f24236f617f4f6f5d1c142eccd7fb702c07ad1f
+        <Stack.Screen name="Person" component={Person} options={{headerShown: false}}/>
+        <Stack.Screen name="Install" component={Install} options={{headerShown: false}}/>
+        <Stack.Screen name="InformationDetail" component={InformationDetail} options={{headerShown: false}}/>
+
+        <Stack.Screen name="Information" component={Information} options={{headerShown: false}}/>
+        <Stack.Screen name="Diary" component={Diary} options={{headerShown: false}}/>
+        
+        <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
+        <Stack.Screen name="TranferAccount" component={TranferAccount} options={{headerShown: false}}/> 
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
