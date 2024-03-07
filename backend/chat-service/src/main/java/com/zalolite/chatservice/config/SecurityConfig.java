20e86bea6368c70524deccf2884ed86e28bfdbc4
sonnees2012 <110987763+sonnees@users.noreply.github.com>
2024-03-07 14:57:28 +0700

    init chat-service (#52)

---
+++ backend/chat-service/src/main/java/com/zalolite/chatservice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.zalolite.chatservice.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public HttpClient httpClientReactor() {
+        return HttpClient.create()
+                .resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder(HttpClient httpClient) {
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient));
+    }
+}
