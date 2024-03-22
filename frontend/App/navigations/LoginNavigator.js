a5fdf7988bb08f706db66af721135fafdfe845fd
Lê Hữu Bằng <105543973+bthope@users.noreply.github.com>
2024-03-22 09:27:33 +0700

    Huu bang (#71)
    * Update API Login and Resgiter
    
    * Upadte QRSCreen

--- frontend/App/navigations/LoginNavigator.js
+++ frontend/App/navigations/LoginNavigator.js
@@ -13,7 +13,7 @@ import AddInforScreen from '../screens/AddInforScreen'
 import ConfirmAccountScreen from '../screens/ConfirmAccountScreen'
 import UpdatePhotoScreen from '../screens/UpdatePhotoScreen'
 import ZaloNameScreen from '../screens/ZaloNameScreen'
-
+import SlashScreen from "../screens/SlashScreen";
 
 
 
@@ -24,17 +24,20 @@ export default function LoginNavigator() {
   return (
       <Stack.Navigator>
           {/* Screen Của Login */}
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ConfirmForgotPasswordScreen" component={ConfirmForgotPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="CreatePasswordScreen" component={CreatePasswordScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="CreatePasswordWhenForgotScreen" component={CreatePasswordWhenForgotScreen} options={{ headerShown: false }} />
+          <Stack.Screen name='SlashScreen' component={SlashScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ headerShown: false }}/>
+          {/* <Stack.Screen name="ConfirmForgotPasswordScreen" component={ConfirmForgotPasswordScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="CreatePasswordScreen" component={CreatePasswordScreen} options={{headerShown: false}}/>
+          <Stack.Screen name="CreatePasswordWhenForgotScreen" component={CreatePasswordWhenForgotScreen} options={{ headerShown: false }} /> */}
           {/* Screen của Register */}
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false}}/>
         <Stack.Screen name="AddInforScreen" component={AddInforScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ConfirmAccountScreen" component={ConfirmAccountScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false}}/>
+        
+        {/* <Stack.Screen name="ConfirmAccountScreen" component={ConfirmAccountScreen} options={{ headerShown: false }} />
         <Stack.Screen name="UpdatePhotoScreen" component={UpdatePhotoScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ZaloNameScreen" component={ZaloNameScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ZaloNameScreen" component={ZaloNameScreen} options={{ headerShown: false }} /> */}
+
+
       </Stack.Navigator>
   );
 }
--- frontend/App/screens/RegisterScreen.js
+++ frontend/App/screens/RegisterScreen.js
@@ -1,42 +1,102 @@
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Alert } from 'react-native';
-import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
+import { CheckBox } from 'react-native-elements';
+import axios from 'axios';
+import { useRoute } from '@react-navigation/native';
 
 export default function RegisterScreen() {
-  const navigation = useNavigation();
-  const [userName, setUserName] = useState(''); 
+  let navigation = useNavigation();
+  const [isChecked, setIsChecked] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('123');
+  
+  const route = useRoute();
+  const userName = route.params.userName;
+
+  const [gender, setGender] = useState(true);
+  const [birthday, setBirthday] = useState('2024-01-26');
+  const [role, setRole] = useState('USER');
+    const handleRegister = () => {
+    // Kiểm tra thông tin nhập vào
+    if (!phoneNumber.trim() || !password.trim() || !userName.trim()) {
+      Alert.alert('Lỗi', 'Vui lòng nhập đầy đủ thông tin');
+      return;
+    }
+
+    // Gửi yêu cầu đăng ký
+    axios.post('http://localhost:8081/api/v1/auth/register', {
+      phoneNumber,
+      password,
+      userName,
+      gender,
+      birthday,
+      role
+    })
+      .then(response => {
+        navigation.navigate('TabNavigator');
+      })
+      .catch(error => {
+        Alert.alert('Lỗi', 'Đăng ký không thành công. Vui lòng thử lại sau.');
+        console.error('Đăng ký không thành công:', error);
+      });
+  };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 2 }} behavior="padding">
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"} 
+    >
       <View style={styles.container}>
-        <View style={{ flex: 0.9, backgroundColor: "#1E90FF", flexDirection: "row", alignItems: "center" }}>
+        <View style={{ flex: 0.6, backgroundColor: "#1E90FF", flexDirection: "row", alignItems: "center" }}>
           <Image
             style={{ width: "15%", height: "40%", resizeMode: "contain" }}
             source={require("../assets/back1.png")}
-            onStartShouldSetResponder={() => navigation.navigate("SlashScreen")}
+            onStartShouldSetResponder={() => navigation.navigate("AddInforScreen")}
           ></Image>
           <Text style={{ fontSize: 16, fontWeight: "bold", fontFamily: "Roboto", color: "white" }}>Tạo tài khoản</Text>
         </View>
-        <View style={{ flex: 6, paddingLeft: "5%", paddingRight: "5%", top: "2%" }}>
-          <Text style={{ fontFamily: "Roboto", fontSize: 16, fontWeight: "bold" }}>Tên ZaloLife</Text>
-          <TextInput
-            style={{ borderBottomWidth: 1, borderBottomColor: "#1E90FF", fontSize: 16, fontFamily: "Roboto", top: "4%", padding: 10}}
-            placeholder='Gồm 2-40 kí tự'
-            value={userName}
-            onChangeText={text => setUserName(text)} 
-          />
-          
-          <Text style={{ fontFamily: "Roboto", fontSize: 16, top: "10%", fontWeight: '700' }}>Lưu ý khi đặt tên:</Text>
-          <Text style={{ fontFamily: "Roboto", fontSize: 14, top: "14%", marginLeft: "6%", fontWeight: '700' }}>• Không vi phạm Quy định đặt tên trên zalo</Text>
-          <Text style={{ fontFamily: "Roboto", fontSize: 14, top: "18%", fontWeight: '700', marginLeft: "6%" }}>• Nên sử dụng tên thật để giúp bạn bè dễ nhận ra bạn</Text>
+        <View style={{ flex: 0.6, backgroundColor: "#EEEEEE", justifyContent: "center" , }}>
+            <Text style={{ fontFamily: "Roboto", fontSize: 14, fontWeight: "700", marginLeft: "3%" }}>Nhập số điện thoại của bạn để tạo tài khoản mới</Text>
         </View>
-        <View style={{ flex: 5 }}></View>
-        <View style={{flex: 2, justifyContent: "center", paddingLeft: "70%" }}>
-          <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}
-            onPress={() => navigation.navigate("RegisterACcountScreen", { userName: userName })} 
-          >
-            <Image style={{width: "100%", height: "50%", resizeMode: "contain"}} source={require("../assets/right-arrow.png")}></Image>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            style={{
+              borderBottomWidth: 1,
+              borderBottomColor: "#1E90FF",
+              fontSize: 16,
+              fontFamily: "Roboto",
+              top: "4%",
+              padding: 10,
+              marginLeft: "3%",
+              marginRight: "3%"
+            }}
+            placeholder='Nhập số điện thoại'
+            value={phoneNumber}
+            onChangeText={text => setPhoneNumber(text)}
+          />
+
+          <CheckBox
+            checked={isChecked}
+            onPress={() => setIsChecked(!isChecked)}
+            title='Tôi đồng ý với điều khoản sử dụng của Zalo'
+            containerStyle={{ marginLeft: "1%", marginTop: "3%", backgroundColor: "transparent", borderWidth: 0 }}
+            textStyle={{ fontFamily: "Roboto", fontSize: 13, fontWeight: '700', color: "#1E90FF" }}
+          />
+
+          <CheckBox
+            checked={isChecked}
+            onPress={() => setIsChecked(!isChecked)}
+            title='Tôi đồng ý với điều khoản Mạng xã hội của Zalo'
+            containerStyle={{ marginLeft: "1%", marginTop: "1%", backgroundColor: "transparent", borderWidth: 0 }}
+            textStyle={{ fontFamily: "Roboto", fontSize: 13, fontWeight: '700', color: "#1E90FF" }}
+          />
+        </View>
+
+         <View style={{ flex: 5 }}></View>
+         <View style={{flex: 2, justifyContent: "center", paddingLeft: "70%" }}>
+          <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}} onPress={handleRegister}>
+            <Image style={{width: "70%", height: "40%", resizeMode: "contain"}} source={require("../assets/right-arrow.png")}></Image>
           </TouchableOpacity>
         </View>
       </View>
