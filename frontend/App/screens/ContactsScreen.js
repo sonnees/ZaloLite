a6faffd545680db7f32522e36950c6f3545c7a85
TranThienDatLaGi <134665779+TranThienDatLaGi@users.noreply.github.com>
2024-03-27 14:47:26 +0700

    change somethings

--- frontend/App/screens/ContactsScreen.js
+++ frontend/App/screens/ContactsScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, TextInput, KeyboardAvoidingView, StyleSheet, Platform, TouchableOpacity, Image, ScrollView,Text,StatusBar } from 'react-native';
+import { View, TextInput, KeyboardAvoidingView, StyleSheet, Platform, TouchableOpacity, Image, ScrollView, Text, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native'
-import Navbar from '../layout/Navbar';
 const YourComponent = () => {
   let navigation = useNavigation();
   return (
@@ -11,26 +10,42 @@ const YourComponent = () => {
       behavior={Platform.OS === 'ios' ? 'padding' : null}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0} // Điều chỉnh offset nếu cần thiết
     >
-      <StatusBar  />
-        <View style={{ flex: 1 }}>
-          <Navbar type={"ContactsScreen"}></Navbar>
+      <StatusBar />
+      <View style={{ flex: 1 }}>
+        <View style={{ backgroundColor: "#1E90FF", flexDirection: "row", justifyContent: "center", alignItems: "center", height: 48 }}>
+          <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", marginLeft: 14 }}>
+            <Icon name='search1' size={22} color={'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 7, borderRadius: 5, backgroundColor: "transparent", height: 40, width: 300, justifyContent: "center", alignItems: "flex-start" }}
+            onPress={() => navigation.navigate("SearchScreen")}
+          >
+            <Text style={{ marginLeft: 20, fontSize: 15.5, color: "#CCCCCC" }}>Search</Text>
+          </TouchableOpacity>
 
-          <View style={{ flexDirection: "row", justifyContent: "center", alignContent: "space-between",paddingVertical: 12,backgroundColor: "#fff",height:45}}>
-              <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
-                <Text style={{fontSize:16}}> Friends</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
-                <Text style={{fontSize:16}}> Groups</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
-                <Text style={{fontSize:16}}> OA</Text>
-              </TouchableOpacity>
-          </View>
-            <View style={{borderBottomColor: 'gray',borderBottomWidth: 1,width: '100%',}} />
-          <View style={{ flex: 1, backgroundColor: "#fff" }}>
-            {/* Your remaining content */}
-          </View>
+
+          <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "flex-end", marginRight: 12 }}
+            onPress={() => { setModalVisible(true) }}
+          >
+            <Icon name='adduser' size={23} color={'white'} />
+          </TouchableOpacity>
         </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "center", alignContent: "space-between", paddingVertical: 12, backgroundColor: "#fff", height: 45 }}>
+          <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 16 }}> Friends</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 16 }}> Groups</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 16 }}> OA</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ borderBottomColor: 'gray', borderBottomWidth: 1, width: '100%', }} />
+        <View style={{ flex: 1, backgroundColor: "#fff" }}>
+          {/* Your remaining content */}
+        </View>
+      </View>
     </KeyboardAvoidingView>
   );
 };
--- frontend/App/screens/TimelineScreen.js
+++ frontend/App/screens/TimelineScreen.js
@@ -1,45 +1,67 @@
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity,StatusBar } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, StatusBar } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Icon from 'react-native-vector-icons/AntDesign';
-import Navbar from '../layout/Navbar';
 
 export default function Diary() {
   let navigation = useNavigation();
   return (
     <View style={styles.container}>
       <StatusBar></StatusBar>
-      <Navbar type={'TimelineScreen'}></Navbar>
-      
-      <View style={{flex: 2, backgroundColor: "#FFFFFF", justifyContent: "center"}}>
-        <View style={{flex: 0.75, flexDirection: "row", alignItems: "center"}} > 
-          <View style={{flex: 0.1}}></View>
-          <View style={{flex: 0.1}}></View>
-            <Text style={{fontFamily: "Roboto", fontSize: 18, color: "#808080"}}>Hôm nay bạn như thế nào?</Text>
+      <View>
+        <View style={{ backgroundColor: "#1E90FF", flexDirection: "row", justifyContent: "center", alignItems: "center", height: 48 }}>
+          <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", marginLeft: 14 }}>
+            <Icon name='search1' size={22} color={'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 7, borderRadius: 5, backgroundColor: "transparent", height: 40, width: 300, justifyContent: "center", alignItems: "flex-start" }}
+            onPress={() => navigation.navigate("SearchScreen")}
+          >
+            <Text style={{ marginLeft: 20, fontSize: 15.5, color: "#CCCCCC" }}>Search</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", marginRight: 18 }}
+            onPress={() => navigation.navigate('MeNavigator', { screen: 'QRScreen' })}
+          >
+            <Image style={{ width: 22, height: 22, resizeMode: "contain" }} source={require("../assets/image.png")} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", marginRight: 10 }}
+
+          >
+            <Image style={{ width: 22, height: 22, resizeMode: "contain" }} source={require("../assets/bell.png")} />
+          </TouchableOpacity>
         </View>
       </View>
-      <View style={{flex: 0.3}}></View>
-      <View style={{flex: 3, backgroundColor: "#FFFFFF"}}></View>
-      <View style={{flex: 0.3}}></View>
 
-      <View style={{flex: 7.95, backgroundColor: "#FFFFFF"}}></View>
-      
-     
-      <View style={{flex: 0.3}}></View>
-      <View style={{flex: 1.5, backgroundColor: "#FFFFFF", flexDirection: "row"}}>
-        <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
+      <View style={{ flex: 2, backgroundColor: "#FFFFFF", justifyContent: "center" }}>
+        <View style={{ flex: 0.75, flexDirection: "row", alignItems: "center" }} >
+          <View style={{ flex: 0.1 }}></View>
+          <View style={{ flex: 0.1 }}></View>
+          <Text style={{ fontFamily: "Roboto", fontSize: 18, color: "#808080" }}>Hôm nay bạn như thế nào?</Text>
+        </View>
+      </View>
+      <View style={{ flex: 0.3 }}></View>
+      <View style={{ flex: 3, backgroundColor: "#FFFFFF" }}></View>
+      <View style={{ flex: 0.3 }}></View>
+
+      <View style={{ flex: 7.95, backgroundColor: "#FFFFFF" }}></View>
+
+
+      <View style={{ flex: 0.3 }}></View>
+      <View style={{ flex: 1.5, backgroundColor: "#FFFFFF", flexDirection: "row" }}>
+        <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
         </TouchableOpacity>
-        <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
+        <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
         </TouchableOpacity>
-        <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}>
+        <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
         </TouchableOpacity>
-        <TouchableOpacity style={{flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center"}}
-            onPress={() => navigation.navigate("Person")}
+        <TouchableOpacity style={{ flex: 1, borderRadius: 20, justifyContent: "center", alignItems: "center" }}
+          onPress={() => navigation.navigate("Person")}
         >
         </TouchableOpacity>
-        
-        
-        
+
+
+
       </View>
     </View>
   )
