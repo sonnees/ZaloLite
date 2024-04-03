954c4f8f64d9a7f564c29a4f79e9c83ea801e2f4
ToHieuDong <144570416+ToHieuDong@users.noreply.github.com>
2024-04-03 16:41:51 +0700

    Gnoodd (#85)
    * resetpassword, info user

---
+++ frontend/Web/zalo-lite/src/components/PopupWindow.jsx
@@ -0,0 +1,84 @@
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+
+export default function PopupWindow ({ isOpen, onClose, data, phoneNumber }) {
+    console.log(data);
+    console.log(phoneNumber);
+    const dateTime = new Date(data.birthday);
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen px-4">
+            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <div className="relative bg-white rounded-lg overflow-hidden shadow-xl max-w-md w-full">
+              {/* Content of the popup */}
+
+              <div className='flex items-center p-4'>
+                <p className='flex-grow'>Thông tin tài khoản</p>
+
+                <p className="bg-gray-50 w-10 text-center cursor-pointer" onClick={onClose}>X</p>
+
+              </div>
+
+
+              <div className="relative bg-cover bg-center bg-no-repeat p-4 h-44 mb-20" style={{backgroundImage: `url(${data.background})`}}>
+                <div className="absolute top-48 left-4 transform -translate-y-1/2">
+                  <img className="w-20 h-20 rounded-full border-4 border-white" src={data.avatar} alt="Avatar" />
+                </div>
+                <div className="absolute top-52 left-4 transform -translate-y-1/2  pl-28">
+                  <h2 className=" text-lg font-semibold">{data.userName}</h2>
+                  {/* <p className=" text-sm">Lorem ipsum dolor sit amet</p> */}
+                </div>
+              </div>
+
+
+              <hr className='bg-slate-200 h-1.5'/>
+              
+              <div className='m-3'>
+                <p className='font-semibold'>Thông tin cá nhân</p>
+
+                <div className='border-b border-gray-200'>
+                  <div className='flex p-3'>
+                    <p className='text-gray-700 flex-grow'>Bio</p>
+                    <p className='w-72 text-left text-gray-700'>{data.userName}</p>
+                  </div>
+                  <div className='flex p-3'>
+                    <p className='text-gray-700 flex-grow'>Giới tính</p>
+                    <p className='w-72 text-left text-gray-700'>{data.gender ? 'Nam' : 'Nữ'}</p>
+                  </div>
+                  <div className='flex p-3'>
+                    <p className='text-gray-700 flex-grow'>Ngày sinh</p>
+                    <p className='w-72 text-left text-gray-700'> {dateTime.getDate()} tháng {dateTime.getMonth()+1}, {dateTime.getFullYear()}</p>
+                  </div>
+                  <div className='flex p-3'>
+                    <p className='text-gray-700 flex-grow'>Điện thoại</p>
+                    <p className='w-72 text-left text-gray-700'>{phoneNumber}</p>
+                  </div>
+                  <div className='flex p-3'>
+                    <p className='text-gray-700 flex-grow'>Chỉ bạn bè có lưu số của bạn trong danh bạ máy xem được số<br/> này</p>
+                  </div>
+                </div>
+
+                <div className="mt-6 px-2">
+                    <button
+                        className="w-full transform rounded-md bg-white py-2 font-semibold tracking-wide text-black transition-colors duration-200"
+                        type="submit"
+                        // onClick={() => navigate('/auth/reset-password')}
+                    >
+                      <FontAwesomeIcon className='mx-1' icon={faPenToSquare} />
+                      Cập nhật
+                    </button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
--- frontend/Web/zalo-lite/src/layouts/auth/index.jsx
+++ frontend/Web/zalo-lite/src/layouts/auth/index.jsx
@@ -1,43 +1,214 @@
-// import React from "react";
-// import { Container, Stack } from "@mui/material";
-// import { Navigate, Outlet } from "react-router-dom";
+import { faLock, faMobileScreen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import QR_Test from "./../../assets/QR_Test.png";
+// import { WebSocket } from 'vite';
 
-// import Logo from "../../assets/Images/logo.ico";
-// import { useSelector } from "react-redux";
+export default function AuthLayout() {
+  const [isSelectQR, setIsSelectQR] = useState(true);
+  const navigate = useNavigate();
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [socket, setSocket] = useState(null);
 
-// const AuthLayout = () => {
-//   const { isLoggedIn } = useSelector((state) => state.auth);
+  //=========================================================
+  function isJSON(str) {
+    try {
+      JSON.parse(str);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-//   if (isLoggedIn) {
-//     return <Navigate to={"/app"} />;
-//   }
+//=========================================================
+  function isJSON(str) {
+    try {
+      JSON.parse(str);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+//=========================================================
+  
 
-//   return (
-//     <>
-//       <Container sx={{ mt: 5 }} maxWidth="sm">
-//         <Stack spacing={5}>
-//           <Stack
-//             sx={{ width: "100%" }}
-//             direction="column"
-//             alignItems={"center"}
-//           >
-//             <img style={{ height: 120, width: 120 }} src={Logo} alt="Logo" />
-//           </Stack>
-//           <Outlet />
-//         </Stack>
-//       </Container>
-//     </>
-//   );
-// };
+  useEffect(() => {
+    // Gọi API ở đây
+    const fetchQrCode = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8081/api/v1/auth/authenticate/qr-code",
+        );
+        // Nếu sử dụng axios:
+        // const response = await axios.post('your_api_url_here', { key: 'value' });
 
-// export default AuthLayout;
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+        if (!response.ok) {
+          throw new Error("Failed to fetch QR code");
+        }
 
-export default function index() {
+        const data = await response.json();
+        // console.log(data);
+        setQrCodeUrl("data:image/png;base64," + data.field2); // Thay "qrCodeUrl" bằng trường dữ liệu thực tế từ API
+        // console.log(qrCodeUrl);
+        //=========SOCKET=========
+        const socketLink = data.field1;
+        const newSocket = new WebSocket('ws://localhost:8081/ws/auth/' + data.field1);
+        console.log(data.field1);
+        newSocket.onopen = () => {
+          console.log("WebSocket connected");
+        };
+
+        setSocket(newSocket);
+        return () => {
+          newSocket.close();
+        };
+        //========================
+      } catch (error) {
+        console.error("Error fetching QR code:", error.message);
+      }
+    };
+
+    fetchQrCode();
+  }, []); // useEffect sẽ chạy một lần khi component được render
+
+  useEffect(() => {
+    if (socket) {
+      handleReceiveToken();
+    }
+  });
+//=========================================================
+
+  async function fetchData(link) {
+    let response = await fetch(link);
+    let data = await response.json();
+    return data;
+  }
+
+  //===========================================
+  const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8081/api/v1/auth/authenticate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            phoneNumber: phoneNumber,
+            password: password,
+          }),
+          // body: JSON.stringify({
+          //   "phoneNumber": "0123456789",
+          //   "password": "123"
+          // }),
+        },
+      );
+
+      if (response.ok) {
+        // Xử lý khi API trả về thành công
+        const token = await response.json();
+        console.log(">>>>>>>TOKEN>>>>>>>>>>", token.field);
+        // navigate('/app', {token: token.field});
+        navigate("/app", {
+          state: { token: token.field, phoneNumber: phoneNumber },
+        });
+        console.log("API call successful");
+      } else {
+        // Xử lý khi API trả về lỗi
+        navigate("/auth/login");
+        console.error("API call failed");
+      }
+    } catch (error) {
+      // Xử lý lỗi khi gọi API
+      navigate("/");
+      console.error("Error calling API:", error);
+    }
+  };
+  //===========================================
+  const handleReceiveToken = () => {
+    socket.onmessage = async (event) => {
+      if (isJSON(event.data)) {
+        let data = JSON.parse(event.data);
+        console.log(data);
+        if (data.token != null) {
+          // console.log(data.token);
+          navigate("/app", { token: data.token });
+        } else if (data.connect == "ACCEPT") {
+          let device = navigator.userAgent.match("Windows") ? "Windows" : "MAC";
+          let day = new Date();
+          let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
+          let location = "TP.HCM";
+          socket.send(
+            JSON.stringify({ device: device, time: time, location: location }),
+          );
+          // console.log(socket);
+        }
+      }
+    };
+  };
+
   return (
-    <div>
-      <Outlet />
+    <div className="w-full">
+      <div className="absolute inset-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1280 654"
+          preserveAspectRatio="xMinYMin slice"
+        >
+          <rect x="1" y="1" width="1280" height="654" fill="#e8f3ff" />
+          <path
+            fill="#e8f3ff"
+            d="M1181.68 655C1163.95 469.296 1031.95 86.8402 963 1H1279V655H1181.68Z"
+          />
+          <path
+            fill="#e8f3ff"
+            d="M1.5 142.5C52.5 267 131.5 487 172 653H1.5V142.5Z"
+          />
+          <path
+            fill="#aad6ff"
+            d="M519.5 1.5H685H964.5C1046 135 1167 469 1180 655.5H767.5C704.5 505.5 604.5 304.5 464 148.5L519.5 1.5Z"
+          />
+          <path
+            fill="#d0e4fc"
+            d="M1 144V1.5H519.5C456 189 322.5 475.5 220 652.5H171.5C138.5 509 51.5 262.5 1 144Z"
+          />
+        </svg>
+      </div>
+      <div className="relative flex flex-col overflow-hidden">
+        <div className="">
+          <h1 className="mt-10 p-3 text-center text-6xl font-semibold text-blue-600">
+            Zalo
+          </h1>
+          <h2 className="text-center font-normal">Đăng nhập tài khoản Zalo</h2>
+          <h2 className="text-center font-normal">
+            để kết nối với ứng dụng Zalo Web
+          </h2>
+        </div>
+
+        <div className="mx-auto my-5 w-full bg-white pb-6 shadow-md lg:max-w-[388px]">
+          {/* =================================================================================== */}
+          <Outlet/>
+          {/* =================================================================================== */}
+        </div>
+
+        <div className="m-3 mt-10">
+          <p className=" text-center text-xs text-blue-600">
+            <a className="font-semibold" href="#">
+              Tiếng Việt
+            </a>{" "}
+            <span> </span>
+            <a className="font-thin" href="#">
+              English{" "}
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
