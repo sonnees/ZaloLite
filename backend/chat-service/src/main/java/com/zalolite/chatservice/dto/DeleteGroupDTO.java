6d65ae85c67f9efa2ff03211d7d6dba5437f5cbd
sonnees <sson12131415@gmail.com>
2024-04-03 09:06:38 +0700

    fix change dto

--- backend/chat-service/src/main/java/com/zalolite/chatservice/dto/DeleteGroupDTO.java
+++ backend/chat-service/src/main/java/com/zalolite/chatservice/dto/DeleteGroupDTO.java
@@ -1,17 +1,17 @@
 package com.zalolite.chatservice.dto;
 
-import com.zalolite.chatservice.entity.PersonInfo;
+import com.zalolite.chatservice.dto.enums.TypeGroupMessage;
+import com.zalolite.chatservice.dto.handleGroup.GroupDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor
 @Getter
 @Setter
-public class DeleteGroupDTO extends GroupDTO{
+public class DeleteGroupDTO extends GroupDTO {
     private String idChat;
 
     public DeleteGroupDTO(UUID id, TypeGroupMessage TGM, String idChat) {
