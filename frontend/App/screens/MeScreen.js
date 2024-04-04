aecb1a5e91d2718273470a2137d14b16d178bc9f
TranThienDatLaGi <134665779+TranThienDatLaGi@users.noreply.github.com>
2024-04-04 09:54:58 +0700

    change file to merge master

--- frontend/App/screens/MeScreen.js
+++ frontend/App/screens/MeScreen.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native'; // Thêm useRoute vào đây
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Thêm useRoute vào đây
 import Icon from 'react-native-vector-icons/AntDesign';
-import { StatusBar } from 'react-native';
 
 export default function MeScreen() {
-  let navigation = useNavigation();
-  const route = useRoute();
-
-  const { userName, avatar } = route.params;
-
+  const navigation = useNavigation();
 
 
   return (
     <View style={styles.container}>
-      <StatusBar></StatusBar>
+      <StatusBar />
       <View style={{ backgroundColor: "#1E90FF", flexDirection: "row", justifyContent: "center", alignItems: "center", height: 48 }}>
         <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", marginLeft: 14 }}>
           <Icon name='search1' size={22} color={'white'} />
@@ -35,21 +30,23 @@ export default function MeScreen() {
         </View>
       </View>
 
-      <View style={{ flex: 1.5, backgroundColor: "#FFFFFF", flexDirection: "row", alignItems: "center", }}>
-        <View style={{ flex: 0.75, flexDirection: "row" }}
-          onStartShouldSetResponder={() => navigation.navigate('MeNavigator', { screen: 'ProfileScreen' })}
+      <View style={{ flex: 1.5, backgroundColor: "#FFFFFF", flexDirection: "row", alignItems: "center" }}>
+        <TouchableOpacity style={{ flex: 0.75, flexDirection: "row" }}
+          onPress={() => navigation.navigate('MeNavigator', { screen: 'ProfileScreen' })}
         >
           <View style={{ flex: 0.1 }}></View>
           <Image style={{ width: 50, height: 50, borderRadius: 50, resizeMode: "contain", marginLeft: "5%" }} source={require("../assets/avata.jpg")}></Image>
           <View style={{ marginLeft: "7%" }}></View>
           <View style={{ justifyContent: "center" }}>
-            <Text style={{ fontFamily: "Roboto", fontSize: 18, fontWeight: "bold" }}>{userName}</Text>
+            <Text style={{ fontFamily: "Roboto", fontSize: 18, fontWeight: "bold" }}>Lê Hữu Bằng</Text>
             <Text style={{ fontFamily: "Roboto", fontSize: 15, color: "#808080" }}>Xem trang cá nhân</Text>
           </View>
-        </View>
-        <Image style={{ width: "10%", height: 28, resizeMode: "contain", marginRight: -100 }} source={require("../assets/transfer.png")}
-          onStartShouldSetResponder={() => navigation.navigate('MeNavigator', { screen: 'SwitchAccountScreen' })}
-        ></Image>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, alignItems: "flex-end", marginRight: "5%" }}
+          onPress={() => navigation.navigate('MeNavigator', { screen: 'SwitchAccountScreen' })}
+        >
+          <Image style={{ width: 28, height: 28, resizeMode: "contain" }} source={require("../assets/transfer.png")} />
+        </TouchableOpacity>
       </View>
       <View style={{ flex: 0.2 }}></View>
 
