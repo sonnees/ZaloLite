6b0863c02a5fd70b0fb2c2b68c0123bc6604eecf
TranThienDatLaGi <134665779+TranThienDatLaGi@users.noreply.github.com>
2024-04-05 21:53:27 +0700

    fix

--- frontend/App/navigations/LoginNavigator.js
+++ frontend/App/navigations/LoginNavigator.js
@@ -21,27 +21,27 @@ import OPTLoginScreen from "../screens/OTPLoginScreen";
 
 export default function LoginNavigator() {
   let Stack = createStackNavigator();
-  
+
   return (
-      <Stack.Navigator>
-          {/* Screen Của Login */}
-          <Stack.Screen name='SlashScreen' component={SlashScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name='CreatePasswordWhenForgotScreen' component={CreatePasswordWhenForgotScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name='CreatePasswordScreen' component={CreatePasswordScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name='ConfirmAccountScreen' component={ConfirmAccountScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name='OPTLoginScreen' component={OPTLoginScreen} options={{ headerShown: false }}/>
-          {/* Screen của Register */}
+    <Stack.Navigator>
+      {/* Screen Của Login */}
+      <Stack.Screen name='SlashScreen' component={SlashScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='CreatePasswordWhenForgotScreen' component={CreatePasswordWhenForgotScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='CreatePasswordScreen' component={CreatePasswordScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='ConfirmAccountScreen' component={ConfirmAccountScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='OPTLoginScreen' component={OPTLoginScreen} options={{ headerShown: false }} />
+      {/* Screen của Register */}
 
-        <Stack.Screen name="AddInforScreen" component={AddInforScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="RegisterDEScreen" component={RegisterDEScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="RegisterProfileScreen" component={RegisterProfileScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="OtpScreen" component={OtpScreen} options={{headerShown: false}}/>
-        
-     
+      <Stack.Screen name="AddInforScreen" component={AddInforScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="RegisterDEScreen" component={RegisterDEScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="RegisterProfileScreen" component={RegisterProfileScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="OtpScreen" component={OtpScreen} options={{ headerShown: false }} />
 
 
-      </Stack.Navigator>
+
+
+    </Stack.Navigator>
   );
 }
