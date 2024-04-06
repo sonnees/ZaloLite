1283433bb43c03420ebaf51b97a8d555beb00af7
sonnees <sson12131415@gmail.com>
2024-04-06 14:21:32 +0700

    add log all

--- backend/account-service/src/main/java/com/zalolite/accountservice/controller/AccountController.java
+++ backend/account-service/src/main/java/com/zalolite/accountservice/controller/AccountController.java
@@ -9,6 +9,7 @@ import com.zalolite.accountservice.dto.FieldDTO;
 import com.zalolite.accountservice.entity.Account;
 import com.zalolite.accountservice.entity.Profile;
 import com.zalolite.accountservice.jwt.AuthenticationManager;
+import com.zalolite.accountservice.serialization.JsonConverter;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -34,8 +35,12 @@ public class AccountController {
     private final ObjectMapper objectMapper;
     private final PasswordEncoder passwordEncoder;
     private final WebClient.Builder builder;
+    private final JsonConverter jsonConverter;
+
     @GetMapping("/profile/{phoneNumber}")
     public Mono<ResponseEntity<String>> getProfileByPhoneNumber(@PathVariable String phoneNumber){
+        log.info("### enter get profile by phone number ###");
+        log.info("# {} #", phoneNumber);
         return  ReactiveSecurityContextHolder.getContext()
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
@@ -45,7 +50,7 @@ public class AccountController {
                                 try {
                                     return Mono.just(ResponseEntity.ok(objectMapper.writeValueAsString(account.getProfile(userDetailsPhoneNumber))));
                                 } catch (JsonProcessingException e) {
-                                    log.error("** "+ e);
+                                    log.error("# {} #", e+"");
                                     return Mono.just(ResponseEntity.status(500).body("Error processing JSON"));
                                 }
                             }).switchIfEmpty(Mono.just(ResponseEntity.status(404).body("User not found")));
@@ -54,6 +59,7 @@ public class AccountController {
 
     @GetMapping("/info")
     public Mono<ResponseEntity<String>> getAccount(){
+        log.info("### enter get info account ###");
         return  ReactiveSecurityContextHolder.getContext()
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
@@ -63,7 +69,7 @@ public class AccountController {
                                 try {
                                     return Mono.just(ResponseEntity.ok(objectMapper.writeValueAsString(account)));
                                 } catch (JsonProcessingException e) {
-                                    log.error("** "+ e);
+                                    log.error("# {} #", e+"");
                                     return Mono.just(ResponseEntity.status(500).body("Error processing JSON"));
                                 }
                             }).switchIfEmpty(Mono.just(ResponseEntity.status(403).body("Not authenticate")));
@@ -72,6 +78,8 @@ public class AccountController {
 
     @PostMapping("/change-password")
     public Mono<ResponseEntity<String>> changePassword(@RequestBody AccountChangePassword info){
+        log.info("### enter change password ###");
+        log.info("# {} #", jsonConverter.objToString(info));
         return ReactiveSecurityContextHolder.getContext()
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
@@ -91,29 +99,30 @@ public class AccountController {
     }
 
     @PostMapping("/change-avatar")
-    public Mono<ResponseEntity<String>> changeAvatar(@RequestBody FieldDTO dto){
-        log.info("** changeAvatar {}", dto.getField());
+    public Mono<ResponseEntity<String>> changeAvatar(@RequestBody FieldDTO info){
+        log.info("### enter change avatar in account ###");
+        log.info("# {} #", jsonConverter.objToString(info));
         return ReactiveSecurityContextHolder.getContext()
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
                     String userDetailsPhoneNumber = (String) authentication.getPrincipal();
                     return accountRepository.searchByPhoneNumber(userDetailsPhoneNumber)
                             .switchIfEmpty(Mono.defer(()->{
-                                log.error("**  searchByPhoneNumber");
+                                log.error("# Error: {} userDetailsPhoneNumber: {} #", "Failed in searchByPhoneNumber", userDetailsPhoneNumber);
                                 return Mono.just(ResponseEntity.status(403).body("Error Token"));
                             }).then(Mono.empty()))
                             .flatMap(account -> {
                                 Profile profile = account.getProfile();
-                                profile.setAvatar(dto.getField());
+                                profile.setAvatar(info.getField());
                                 return accountRepository.changeAvatar(userDetailsPhoneNumber, profile)
                                         .flatMap(aLong -> {
                                             if(aLong<=0) {
-                                                log.error("**  changeAvatar");
-                                                return Mono.just(ResponseEntity.status(403).body("Error"));
+                                                log.error("# Error: {} userDetailsPhoneNumber: {} profile: {} #", "Failed in changeAvatar", userDetailsPhoneNumber, jsonConverter.objToString(profile));
+                                                return Mono.just(ResponseEntity.status(500).body("Error"));
                                             }
                                             WebClient webClient = builder.build();
                                             String oldAvatar = account.getProfile().getAvatar();
-                                            String newAvatar = dto.getField();
+                                            String newAvatar = info.getField();
                                             return webClient.get()
                                                     .uri("http://CHAT-SERVICE/api/v1/user/update-avatar-account?oldAvatar="+ oldAvatar+"&newAvatar="+newAvatar)
                                                     .retrieve()
--- backend/chat-service/src/main/java/com/zalolite/chatservice/controller/ChatController.java
+++ backend/chat-service/src/main/java/com/zalolite/chatservice/controller/ChatController.java
@@ -3,48 +3,43 @@ package com.zalolite.chatservice.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zalolite.chatservice.entity.Chat;
-import com.zalolite.chatservice.entity.Content;
-import com.zalolite.chatservice.entity.User;
 import com.zalolite.chatservice.repository.ChatRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/chat")
 @Slf4j
 public class ChatController {
-    private ChatRepository chatRepository;
-    private ObjectMapper objectMapper;
-    private WebClient.Builder builder;
+    private final ChatRepository chatRepository;
+    private final ObjectMapper objectMapper;
 
     @PostMapping("/create")
     public Mono<Boolean> createChat(@RequestParam String id){
-        log.info("** createChat");
+        log.info("### enter create chat ###");
+        log.info("# {} #", id);
         return chatRepository.save(new Chat(id))
                 .flatMap(chat -> Mono.just((chat!=null)));
     }
 
     @GetMapping("/x-to-y")
     public Mono<ResponseEntity<String>> getChatActivityFromNToM(@RequestParam String id, @RequestParam int x, @RequestParam int y){
+        log.info("### enter get chat active from n to m ###");
+        log.info("# id: {} x: {} y: {} #", id, x, y);
         return chatRepository.getChatActivityFromNToM(id,x,y)
                 .collectList()
                 .flatMap(list -> {
                     try {
                         return Mono.just(ResponseEntity.status(200).body(objectMapper.writeValueAsString(list)));
                     } catch (JsonProcessingException e) {
-                        log.error("** "+ e);
+                        log.error("# {} #", e+"");
                         return Mono.just(ResponseEntity.status(500).body("Error processing JSON"));
                     }
                 });
     }
-
-
 
 }
