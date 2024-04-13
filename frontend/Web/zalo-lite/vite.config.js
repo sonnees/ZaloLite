fc61ac8eb25f3d9696f81b57da1f383922b603e3
Tran Quang Huy <tranquanghuyit09@gmail.com>
2024-04-14 00:26:00 +0700

    update vite.config.js

--- frontend/Web/zalo-lite/vite.config.js
+++ frontend/Web/zalo-lite/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: 'auth/login' 
+  },
   plugins: [react()],
   // base: 'auth/login'
 })
