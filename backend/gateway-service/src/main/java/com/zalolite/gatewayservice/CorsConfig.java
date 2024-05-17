20ec3c3b8717311804c6edaf6d414968a0dbb28c
sonnees <sson12131415@gmail.com>
2024-05-17 17:19:19 +0700

    ...

--- backend/gateway-service/src/main/java/com/zalolite/gatewayservice/CorsConfig.java
+++ backend/gateway-service/src/main/java/com/zalolite/gatewayservice/CorsConfig.java
@@ -1,15 +1,10 @@
 package com.zalolite.gatewayservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-
-import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
